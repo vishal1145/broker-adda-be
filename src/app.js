194290707862727848203c7841afa518a4beff5c
muskan <muskan.shukla@algofolks.com>
2025-09-12 15:14:06 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -21,9 +21,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000', 
     'http://localhost:3001', 
-    '*',
-    'http://localhost:9090/',
-    'http://localhost:4189/',
+    'http://localhost:9090',
+    'http://localhost:4189',
     'https://broker-adda.algofolks.com',
     'https://admin.broker-adda.algofolks.com',
     'https://broker-adda-be.algofolks.com',
