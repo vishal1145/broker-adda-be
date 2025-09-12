e88f588c4fae536be9cbd8d60d5086dd776e92d0
muskan <muskan.shukla@algofolks.com>
2025-09-12 17:36:58 +0530

    change

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(cors({
     /\.algofolks\.com$/  // Allow all subdomains of algofolks.com
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
   exposedHeaders: ['Content-Disposition'],
   optionsSuccessStatus: 200 // Some legacy browsers choke on 204
@@ -44,7 +44,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Max-Age', '86400'); // 24 hours
